739c6af1f581c185021a4ef2a9f79eb74085edd4
Taheo <segieta.michal@gmail.com>
2016-10-23 14:29:30 +0200

    initialize

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2.napisz apke do lcizenia pola powierzchi oraz objetosci bryl takich jak stozek, walec, kula, prostopadloscian, ostroslup o podstawie prostokatnej
+            // sklad programu powinno wchodzic 
+            // generowanie menu z wyborem bryly
+            // po zakonczeniu obliczen menu ponownie ma byc wyswietlone
+            // wsyzstkei dane wczytujemy od uzytkownika
+            // wsrod opcji ma byc taka ktora pozwala na zamkniecie apki
+            // pole powierzchni ma byc wyznaczane za pomoca dedykowanych metod dla kazdej byly
+            //------
+        }
+    }
+}
