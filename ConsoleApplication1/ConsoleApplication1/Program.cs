5ab9712d1fb5b99421fc2c3d0e13f4c94ab129d2
Taheo <segieta.michal@gmail.com>
2016-10-23 15:04:51 +0200

    added menu

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -8,8 +8,21 @@ namespace ConsoleApplication1
 {
     class Program
     {
+        public static void RysujMenu()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Menu wyboru");
+            Console.WriteLine("1. Stożek ");
+            Console.WriteLine("2. Walec");
+            Console.WriteLine("3. Kula");
+            Console.WriteLine("4. Prostpoadłościan");
+            Console.WriteLine("5. Ostrosłup o podstawie prostokątnej");
+            Console.WriteLine("");
+        }
+
         static void Main(string[] args)
         {
+            RysujMenu();   
             //2.napisz apke do lcizenia pola powierzchi oraz objetosci bryl takich jak stozek, walec, kula, prostopadloscian, ostroslup o podstawie prostokatnej
             // sklad programu powinno wchodzic 
             // generowanie menu z wyborem bryly
@@ -18,6 +31,8 @@ namespace ConsoleApplication1
             // wsrod opcji ma byc taka ktora pozwala na zamkniecie apki
             // pole powierzchni ma byc wyznaczane za pomoca dedykowanych metod dla kazdej byly
             //------
+
+      
 
         }
     }
